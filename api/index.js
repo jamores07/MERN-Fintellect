f71f1161a0cc2562a23f0aa2ce7aba3b8f26e762
John A <kayak.stated_0b@icloud.com>
2024-03-09 03:13:37 +0000

    Vercel Test 01

--- api/index.js
+++ api/index.js
@@ -1,24 +1,33 @@
 import express from "express";
+import bodyParser from "body-parser";
+//import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
 
-// Add Express
-const express = require("express");
-
-// Initialize Express
+/* Configurations */
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 // Create GET request
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
-});
+    res.send("Express on Vercel");
+  })
 
-// Initialize server
-const port = process.env.PORT || 9000;
+/* Server */
+const port = process.env.PORT || 9001;
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+    console.log(`Server is listening on port ${port}`);
+  });
 
-//Export Express API
+// Export the Express API
 module.exports = app;
