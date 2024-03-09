011afd197bc4ec8460804cb5e9231ccc3d20e901
John A <kayak.stated_0b@icloud.com>
2024-03-09 04:07:25 +0000

    Vercel Test 08

--- api/index.js
+++ api/index.js
@@ -1,20 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 /* Configurations */
-
 const app = express();
 
 // Create GET request
 app.get("/", (req, res) => {
-    res.send(`"Express on Vercel and listening on port ${port}`);
+    res.send(`"Express on Vercel and listening on port ${PORT}"`);
   })
 
 /* Server */
-const port = process.env.PORT || 9001;
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
 
 // Export the Express API
 module.exports = app;
