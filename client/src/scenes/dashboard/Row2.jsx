509e55716e6919bcf3659b1e3a96321bbeee0417
John A <kayak.stated_0b@icloud.com>
2024-03-18 06:03:31 +0000

    Client folder deleted

--- client/src/scenes/dashboard/Row2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Row2 = () => {
-  return (
-    <div>Row1</div>
-  )
-}
-
-export default Row2
--- client/src/scenes/dashboard/Row3.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Row3 = () => {
-  return (
-    <div>Row3</div>
-  )
-}
-
-export default Row3
