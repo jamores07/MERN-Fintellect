57cdeda1683a5ee2a47aef34f072cf0d2b3a4770
John A <kayak.stated_0b@icloud.com>
2024-03-20 23:30:34 +0000

    Navbar added.

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,6 +1,6 @@
 import { Box, useMediaQuery } from "@mui/material";
-//import Row1 from "./Row1";
-import Row2 from "./Row2";
+import Row1 from "./Row1";
+
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -71,7 +71,7 @@ const Dashboard = () => {
           }
     }
   >
-    <Row2 />
+    <Row1 />
 
   </Box>
   );
