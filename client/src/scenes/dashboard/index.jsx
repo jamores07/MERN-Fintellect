e5391c52463e20bb6419e66b5b19d0a318f35421
John A <kayak.stated_0b@icloud.com>
2024-03-22 01:29:38 +0000

    Dev code -> Main cleanup -- Api call

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,9 +1,6 @@
 import { Box, useMediaQuery } from "@mui/material";
 import Row1 from "./Row1";
-<<<<<<< HEAD
-=======
 //import Row2 from "./Row2";
->>>>>>> development
 
 
 const gridTemplateLargeScreens = `
