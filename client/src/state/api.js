dfa8f4ac4951629eee2c0330354391eab391e5f4
John A <kayak.stated_0b@icloud.com>
2024-03-23 23:37:12 +0000

    Added development API

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: "https://mern-fintellect-client.vercel.app/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5173/" }),
     tagTypes: ["Kpis", "Products", "Transactions"],
     endpoints: (build) => ({
         getKpis: build.query({
