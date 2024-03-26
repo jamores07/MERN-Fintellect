167097d1f34cdf81c2c26111e6b8ab08df1b003f
John A <kayak.stated_0b@icloud.com>
2024-03-26 01:50:43 +0000

    Prediction Page completed - Vercel Deployment

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5173/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://mern-fintellect-client.vercel.app/" }),
     tagTypes: ["Kpis", "Products", "Transactions"],
     endpoints: (build) => ({
         getKpis: build.query({
